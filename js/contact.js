c279e5fc38a0fcb300bdb1c5ac333357123f1e0f
bkdobosh <bkdobosh@earthlink.net>
2015-12-20 12:16:54 -0500

    Updated

---
+++ js/contact.js
@@ -0,0 +1,15 @@
+var contact = [
+
+{"name" : "Justin",
+"phoneNumber" : "2033123846",
+"emailAddress" : "jfactorteam@gmail.com"
+},
+{"name" : "Brian",
+"phoneNumber" : "2033124177",
+"emailAddress" : "bkdobosh@earthlink.net"
+}
+];
+
+console.log (contact [1].name);
+console.log (contact [1].phoneNumber);
+console.log (contact [1].emailAddress);
