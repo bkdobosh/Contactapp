9ce3e44f92433cf31d6704d2e9d3a2b9cd6529d1
bkdobosh <bkdobosh@earthlink.net>
2015-12-23 07:51:46 -0500

    update

--- js/contact.js
+++ js/contact.js
@@ -524,7 +524,12 @@ console.log (contact [1].emailAddress);
 
 	var listContainer = document.createElement("ul");
 
+function addContacts() {
+	var name = $("#nameInfo").val();
+	var email = $("#emailInput").val();
+	var phone = $("#phoneNumber").val();
 
+	contact.push({"name" : name, "email" : email, "cell" : phone});
 
 	var nameInfo = contact[0].name;
 	var emailInfo = contact[0].email;
